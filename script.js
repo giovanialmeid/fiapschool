907761b319d581e5dfada0b0e760cab77cf12a2d
giovanialmeid <137239551+giovanialmeid@users.noreply.github.com>
2023-09-23 23:06:11 -0300

    Adicionando o html css e js
    Adicionando o html css e js da ac

---
+++ script.js
@@ -0,0 +1,24 @@
+const botaocpu = document.getElementById('cpu');
+botaocpu.addEventListener('click', function() {
+    alert('Ao clicar no botão que liga o PC, o processador fica responsavel por executar programas/softwares (Windows ou outro sistema operacional) e até outras atividades que não percebemos.');
+});
+
+const botaoram = document.getElementById('ram');
+botaoram.addEventListener('click', function() {
+    alert('Ao clicar no botão que liga o pc, a memória RAM desempenha um papel vital no funcionamento do computador, proporcionando armazenamento temporário de dados e programas para acesso rápido pela CPU. Isso melhora a velocidade e a eficiência do sistema, garantindo que as operações ocorram de forma suave e responsiva desde o momento em que o PC é ligado.');
+});
+
+const botinterno = document.getElementById('interno');
+botinterno.addEventListener('click', function() {
+    alert('Ao clicar no botão que liga o pc, o armazenamento interno é onde seu dispositivo guarda permanentemente dados e programas, como o sistema operacional e aplicativos. É diferente da memória RAM, pois mantém os dados mesmo quando o dispositivo está desligado. Os principais tipos são HDDs e SSDs.');
+});
+
+const botaoplacamae = document.getElementById('placamae');
+botaoplacamae.addEventListener('click', function() {
+    alert('Ao clicar no botão que liga o PC, a placa mãe fica responsavel por basicamente tudo, pois sem ela não teria uma forma de outros componentes se comunicarem ou seja sem ela não teria como ligar o computador.');
+});
+
+const botaovideo = document.getElementById('placavideo');
+botaovideo.addEventListener('click', function() {
+    alert('Ao clicar no botão que liga o pc, a placa de vídeo é um componente que processa gráficos e exibe imagens na tela do seu dispositivo. Ela é essencial para jogos, vídeos e gráficos. As placas de vídeo possuem sua própria memória (VRAM) para armazenar texturas e quadros temporariamente, o que melhora o desempenho gráfico. Elas se conectam à placa-mãe e ao monitor, e podem ser integradas à placa-mãe ou dedicadas (placas de vídeo separadas). As placas de vídeo dedicadas oferecem melhor desempenho em comparação com as integradas, sendo preferidas por gamers e profissionais de design.');
+});
